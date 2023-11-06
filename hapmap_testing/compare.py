e663d59e9aff2a1080e716ad0cbb8056a8b20226
adityapanchal10 <panchaladitya007@gmail.com>
2023-11-06 15:10:19 +0100

    dasfafs

--- hapmap_testing/compare.py
+++ hapmap_testing/compare.py
@@ -10,24 +10,20 @@ def compare(f1, f2): # f1 is test, f2 is truth
         for i, (line1, line2) in enumerate(zip(file1_lines, file2_lines)):
             line1 = line1.strip().split()
             line2 = line2.strip().split()
-            if f1.endswith(".hap"):
-                hap = []
-                for item in line1:
-                    hap.extend((list(item)))
-                line1 = hap
             if f1.endswith(".par"):
+                line1 = line1[0]
                 line2 = line2[0]
             if line1 != line2:
                 identical = False
-                print(f"Files differ at line {i+1}:")
-                print(f"File 1: {line1.strip()}")
-                print(f"File 2: {line2.strip()}")
+                print(f"Files {f1} and {f2} differ at line {i+1}:")
+                print(f"File 1 {f1}: {line1}")
+                print(f"File 2 {f2}: {line2}")
                 print("-------------")
                 #break
         if identical:
             print(f"Files {f1} and {f2} are identical")
 
-for i in [".hap", ".map", ".par"]:
+for i in [".Hap", ".Map", ".par"]:
     # f1 is test, f2 is truth
     f1 = sys.argv[1] + i
     f2 = sys.argv[2] + i
